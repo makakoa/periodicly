efc3362b58c88e35488f84ab05b57dee92ed66f0
Cameron Yee <makakoa57@gmail.com>
2017-05-28 18:54:52 -0700

    hall of fame view

--- scripts.js
+++ scripts.js
@@ -1,54 +1,122 @@
+// reference point
+var ref = { x: 0, y: 0, z: -1000, rx: 0, ry: 0, rz: 0 };
+
+// main
+var main = document.getElementById('main');
+function centerMain() {
+  main.style.transform = [
+    'translate(', window.innerWidth / 2, 'px,', window.innerHeight / 2,'px)'
+  ].join('');
+}
+window.onresize = centerMain;
+centerMain();
+document.body.addEventListener('click', defaultView);
+
+// Item input (generation)
 function randomItem(a) {
   return a[Math.floor(Math.random() * a.length)];
 }
-// Item generation
 var items = Array(118);
+// var items = Array(118);
 for (var i = 0; i < items.length; i++) {
   items[i] = {
     id: i,
     x: 0,
     y: 0,
     z: 0,
+    rx: 0,
+    ry: 0,
+    rz: 0,
     text: randomItem('ABCDEFGHIJKLMNOPQRSTUVWXYZ') + randomItem('aeiouy')
   };
 }
 
-// main
-var main = document.getElementById('main');
-set3d(main, window.innerWidth / 2, window.innerHeight / 2, 0, 0);
-document.body.addEventListener('click', function() {
-  defaultView();
-});
-
-// reference
-var refEl = document.getElementById('reference');
-var reference = {el: refEl, x: 0, y: 0, z: -1000};
-function lookAt(x, y, z) {
-  reference.x = -x;
-  reference.y = -y;
-  reference.z = 200 + z;
-}
-
+// create items
 items.forEach(function(item) {
   var el = document.createElement('div');
   el.className = 'item';
-  el.innerText = item.text + item.id;
-  refEl.appendChild(el);
+  el.innerText = item.text + (item.id + 1);
+  main.appendChild(el);
   el.addEventListener('click', function(e) {
+    console.log('click', item);
     lookAt(item.x + 70, item.y + 90, item.z);
     e.stopPropagation();
   });
   item.el = el;
 });
 
-var currentStructure = 'table';
-var tCols = 18;
+// Structure logic
+var currentStructure = 'halloffame';
 var structures = {
-  table: {
+  helix: {
+    radius: 1000,
+    perLoop: 30,
+    stretch: 10,
     resetView: function() {
-      reference.x = -1250; // middle x
-      reference.y = -900; // middle y
-      reference.z = -1000;
+      ref.x = 0;
+      ref.y = -(this.stretch * items.length / 2);
+      ref.z = -1000;
+      ref.rx = 0;
+      ref.ry = 0;
+      ref.rz = 0;
+    },
+    layout: function() {
+      var rowLength = this.perLoop;
+      var counter = 0;
+      while (items[counter]) {
+        var theta = Math.floor(((counter / rowLength) % 1) * 360);
+        var cosTheta = Math.cos(theta / 180 * Math.PI);
+        var sinTheta = Math.sin(theta / 180 * Math.PI);
+        var r = this.radius;
+        items[counter].x = r * cosTheta;
+        items[counter].y = counter * this.stretch;
+        items[counter].z = r * sinTheta;
+        items[counter].rx = 0;
+        items[counter].ry = 90 - theta;
+        items[counter].rz = 0;
+        counter++;
+      }
+    }
+  },
+  halloffame: {
+    radius: 1000,
+    rows: 4,
+    resetView: function() {
+      ref.x = 0;
+      ref.y = -(this.rows * 90);
+      ref.z = -1000;
+      ref.rx = 0;
+      ref.ry = 0;
+      ref.rz = 0;
+    },
+    layout: function() {
+      var rowLength = Math.ceil(items.length / this.rows);
+      
+      var counter = 0;
+      while (items[counter]) {
+        var theta = Math.floor(((counter / rowLength) % 1) * 360);
+        var cosTheta = Math.cos(theta / 180 * Math.PI);
+        var sinTheta = Math.sin(theta / 180 * Math.PI);
+        var r = this.radius;
+        items[counter].x = r * cosTheta;
+        items[counter].y = Math.floor(counter / rowLength) * 180;
+        items[counter].z = r * sinTheta;
+        items[counter].rx = 0;
+        items[counter].ry = 90 - theta;
+        items[counter].rz = 0;
+        counter++;
+      }
+    }
+  },
+  table: {
+    cols: 18,
+    resetView: function() {
+      ref.x = -1250; // middle x
+      ref.y = -900; // middle y
+      ref.z = -2000;
+      ref.rx = 0;
+      ref.ry = 0;
+      ref.rz = 0;
     },
     layout: function() {
       
@@ -64,11 +132,15 @@ var structures = {
       var counter = 0;
       var p = 0;
       while (items[counter]) {
-        while (p < 200 && !grid[Math.floor(p / tCols)][p % tCols]) {
+        while (p < 200 && !grid[Math.floor(p / this.cols)][p % this.cols]) {
           p++;
         }
-        items[counter].x = (p % tCols) * 140; // col width
-        items[counter].y = (Math.floor(p / tCols)) * 180; // row height
+        items[counter].x = (p % this.cols) * 140; // col width
+        items[counter].y = (Math.floor(p / this.cols)) * 180; // row height
+        items[counter].z = 0;
+        items[counter].rx = 0;
+        items[counter].ry = 0;
+        items[counter].rz = 0;
         counter++;
         p++;
       }
@@ -76,46 +148,76 @@ var structures = {
   }
 };
 
-
-
-structures[currentStructure].layout();
-structures[currentStructure].resetView();
-
+function setStructure(type) {
+  structures[type].layout();
+  structures[type].resetView();
+  currentStructure = type;
+}
 
 function defaultView() {
   structures[currentStructure].resetView();
 }
 
+function lookAt(x, y, z) {
+  ref.x = -x;
+  ref.y = -y;
+  ref.z = 200 + z;
+}
+
+function renderItem(item) {
+  set3d(item.el, item);
+}
+
+function set3d(el, d) {
+  var x = ref.x + d.x;
+  var y = ref.y + d.y;
+  var z = ref.z + d.z;
+  el.style.transform = [
+    'perspective(1000px) ',
+    'translate3d(', x, 'px,', y, 'px,', z, 'px) ',
+    'rotateX(' + ((d.rx || 0)) + 'deg) ',
+    'rotateY(' + ((d.ry || 0)) + 'deg) ',
+    'rotateZ(' + ((d.rz || 0)) + 'deg) '
+  ].join('');
+}
+
+// Begin
+setStructure(currentStructure);
+
+// dev
+var originEl = document.createElement('div');
+originEl.innerText = 'origin';
+originEl.style.backgroundColor = 'white';
+main.appendChild(originEl);
+var origin = {el: originEl, x: 0, y: 0, z: 0, rx: 0, ry: 0, rz: 0};
+
+// use requestAnimationFrame?
+setInterval(function() {
+  renderItem(origin);
+  items.forEach(renderItem);
+}, 1000 / 30); // 30 fps
+
+
+// Controls
 var speed = 100;
 document.addEventListener('keydown', function (event) {
   var k = event.key;
-
-  if (k === 'ArrowUp') {
-    reference.z -= speed;
-  } else if (k === 'ArrowDown') {
-    reference.z += speed;
-  } else if (k === 'w') {
-    reference.y += speed;
-  } else if (k === 'a') {
-    reference.x += speed;
-  } else if (k === 's') {
-    reference.y -= speed;
-  } else if (k === 'd') {
-    reference.x -= speed;
-  }
+  if (k === 'ArrowUp') ref.z -= speed;
+  if (k === 'ArrowDown') ref.z += speed;
+  if (k === 'w') ref.y += speed;
+  if (k === 'a') ref.x += speed;
+  if (k === 's') ref.y -= speed;
+  if (k === 'd') ref.x -= speed;
+  if (k === 'e') ref.ry += speed / 36;
+  if (k === 'q') ref.ry -= speed / 36;
 }, false);
 
-
-function renderItem(item) {
-  set3d(item.el, item.x, item.y, item.z);
-}
-
-function set3d(el, x, y, z, p) {
-  el.style.transform = 'perspective(' + (p || 500) + 'px) translate3d(' + x
-    + 'px,' + y + 'px,' + z +'px)';
-}
-
-setInterval(function() {
-  renderItem(reference);
-  items.forEach(renderItem);
-}, 1000 / 30); // 30 fps
+document.getElementById('table-view').addEventListener('click', function() {
+  setStructure('table');
+});
+document.getElementById('hall-of-fame-view').addEventListener('click', function() {
+  setStructure('halloffame');
+});
+document.getElementById('helix-view').addEventListener('click', function() {
+  setStructure('helix');
+});
